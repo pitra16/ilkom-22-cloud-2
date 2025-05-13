46b008196dc1ea059820533fbfded36e764a9b64
loli hunter <mh1225az@gmail.com>
2025-05-13 17:57:01 +0800

    tr

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -115,17 +115,17 @@
                     <div class="product-category" data-background="assets/img/bg/cat_bg.jpg">
                         <h2 class="section-heading mb-25"><span><span>Catagory</span></span></h2>
                         <ul class="list-unstyled">
-                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_01.svg" alt="">Bluetooth speaker</a></li>
-                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_02.svg" alt="">Digital camera</a></li>
-                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_03.svg" alt="">Laser printer</a></li>
-                            <li><a href="#!"><img src="assets/img/icon/cat_04.svg" alt="">Electric frying pan</a></li>
-                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_05.svg" alt="">Robotics vacuum</a></li>
-                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_06.svg" alt="">external hard drive</a></li>
-                            <li><a href="#!"><img src="assets/img/icon/cat_07.svg" alt="">Electric razor</a></li>
-                            <li><a href="#!"><img src="assets/img/icon/cat_08.svg" alt="">Washing machine</a></li>
-                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_09.svg" alt="">Decorative Lights</a></li>
-                            <li><a href="#!"><img src="assets/img/icon/cat_10.svg" alt="">Telivsion & Monitor</a></li>
-                            <li class="more-item"><a href="#!">+ More item</a></li>
+                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_01.svg" alt="">Speaker Bluetooth</a></li>
+                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_02.svg" alt="">Kamera digital</a></li>
+                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_03.svg" alt="">Printer laser</a></li>
+                            <li><a href="#!"><img src="assets/img/icon/cat_04.svg" alt="">Penggorengan listrik</a></li>
+                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_05.svg" alt="">Penyedot debu robotik</a></li>
+                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_06.svg" alt="">hard drive eksternal</a></li>
+                            <li><a href="#!"><img src="assets/img/icon/cat_07.svg" alt="">Pisau cukur listrik</a></li>
+                            <li><a href="#!"><img src="assets/img/icon/cat_08.svg" alt="">Mesin cuci</a></li>
+                            <li class="cat-item-has-children"><a href="#!"><img src="assets/img/icon/cat_09.svg" alt="">Lampu Hias</a></li>
+                            <li><a href="#!"><img src="assets/img/icon/cat_10.svg" alt="">Televisi & Monitor</a></li>
+                            <li class="more-item"><a href="#!">+ Item lainnya</a></li>
                         </ul>
                     </div>
                 </div>
