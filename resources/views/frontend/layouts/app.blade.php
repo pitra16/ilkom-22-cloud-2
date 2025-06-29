aec73d6077367b0efbf62f858c9bf6faa6a85a16
FaizalMuhammad <22muh.izal@gmail.com>
2025-06-29 09:03:00 +0700

    fixed minor details in app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,103 +1,59 @@
 <!doctype html>
-<html lang="zxx">
+<html lang="en">
 
 <head>
-
-    <!--========= Required meta tags =========-->
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="description" content="">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="A modern and responsive electronics store template" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <title>Radios - Electronics Store WooCommerce Template</title>
-
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="images/x-icon"/>
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="images/x-icon" />
 
     <!-- css include -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="assets/css/metisMenu.css">
-    <link rel="stylesheet" href="assOets/css/uikit.min.css">
-    <link rel="stylesheet" href="assets/css/jquery-ui.css">
-    <link rel="stylesheet" href="assets/css/slick.css">
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="assets/css/animate.css" />
+    <link rel="stylesheet" href="assets/css/metisMenu.css" />
+    <link rel="stylesheet" href="assets/css/uikit.min.css" />
+    <link rel="stylesheet" href="assets/css/jquery-ui.css" />
+    <link rel="stylesheet" href="assets/css/slick.css" />
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
 </head>
 
 <body>
-
     <div class="body_wrap">
-
-        <!-- preloder start  -->
-        <div class="preloder_part">
+        <!-- Preloader -->
+        <div class="preloader_part">
             <div class="spinner">
                 <div class="dot1"></div>
                 <div class="dot2"></div>
             </div>
         </div>
-        <!-- preloder end  -->
 
-        <!-- back to top start -->
+        <!-- Back to top -->
         <div class="progress-wrap">
             <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
+                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
             </svg>
         </div>
-        <!-- back to top end -->
 
         @include('frontend.layouts.header')
-
         @include('frontend.layouts.sidebar')
-
         @yield('content')
-
         @include('frontend.layouts.footer')
 
-        {{-- <!-- start newsletter-popup-area-section -->
-        <section class="newsletter-popup-area-section">
-            <div class="newsletter-popup-area">
-                <div class="newsletter-popup-ineer">
-                    <button class="btn newsletter-close-btn"><i class="fal fa-times"></i></button>
-                    <div class="img-holder">
-                        <img src="assets/img/bg/newsletter.jpg" alt>
-                    </div>
-                    <div class="details">
-                        <h4>Get 45% discount shipped to your inbox</h4>
-                        <p>Subscribe to the radios eCommerce newsletter to receive timely updates to your favorite products</p>
-                        <form>
-                            <div>
-                                <input type="email" placeholder="Enter your email" />
-                                <button type="submit">Subscribe</button>
-                            </div>
-                            <div>
-                                <label class="checkbox-holder"> Don't show this popup again!
-                                    <input type="checkbox" class="show-message">
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- end newsletter-popup-area-section --> --}}
-
-
-        <!-- start cookies-area -->
+        <!-- Cookie Consent -->
         <div class="cookies-area">
-            <p> This website uses cookies to improve your experience. By using this website you agree to our <a href="#">Data Protection Policy</a>. </p>
+            <p>This website uses cookies to improve your experience. By using this website, you agree to our <a href="#">Data Protection Policy</a>.</p>
             <a href="#" class="read-more">Read more</a>
             <div>
                 <button class="cookie-btn">Accept</button>
             </div>
         </div>
-        <!-- end cookies-area -->
-
-
     </div>
-
 
     @stack('script')
 
